abf825ad1f3ebf661415fc3b58fd609347433881
JohanVanneuville <74196795+JohanVanneuville@users.noreply.github.com>
2021-11-05 07:57:01 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "location" {
+  description = "(Required) location where this resource has to be created"
+  default = "westeurope"
+}
+variable "prefix" {
+    description = "customer prefix"
+    default = "jvn"
+}
