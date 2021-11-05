56caed96a9430a7f2f72216f339ad586e126edc7
JohanVanneuville <74196795+JohanVanneuville@users.noreply.github.com>
2021-11-05 07:57:30 +0100

    Delete variables.tf

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "location" {
-  description = "(Required) location where this resource has to be created"
-  default = "westeurope"
-}
-variable "prefix" {
-    description = "customer prefix"
-    default = "jvn"
-}
