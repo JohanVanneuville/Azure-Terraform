a20facbca989adb3791145d4f16d066e501b319c
JohanVanneuville <74196795+JohanVanneuville@users.noreply.github.com>
2022-01-26 15:07:06 +0100

    Create variables.tf

---
+++ Modules/Landing Zone/Management Groups/variables.tf
@@ -0,0 +1,39 @@
+variable "fullcompanyname" {
+  default = "johanvanneuville"
+}
+variable "shortcompanyname" {
+  default = "jvn"
+}
+variable "CompanyManagementGroupName" {
+  default = "johanvanneuville"
+}
+variable "decomissioned" {
+  default = "decommisioned"
+}
+variable "landingzones" {
+  default = "landingzones"
+}
+variable "corp" {
+  default = "corp"
+}
+variable "online" {
+  default = "online"
+}
+ variable "sap" {
+     default = "sap"
+ } 
+ variable "platform" {
+     default = "platform"
+ } 
+  variable "connectivity" {
+     default = "connectivity"
+ } 
+ variable "identity" {
+    default = "identity"
+ }
+  variable "management" {
+    default = "management"
+ }
+   variable "sandboxes" {
+    default = "sandboxes"
+ }
