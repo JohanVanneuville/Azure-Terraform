422638cf44f74da86cf031a548a4437b31fc9c74
JohanVanneuville <74196795+JohanVanneuville@users.noreply.github.com>
2022-03-14 07:04:44 +0100

    Update variables.tf

--- Modules/Landing Zone/Hub-Spoke/variables.tf
+++ Modules/Landing Zone/Hub-Spoke/variables.tf
@@ -1,13 +1,4 @@
-variable "location" {
-  description = "(Required) location where this resource has to be created"
-  default = "westeurope"
-}
 variable "prefix" {
     description = "customer prefix"
     default = "jvn"
-}
-variable "environment" {
-    description = "prd,uat"
-    default = "prd"
-  
 }
