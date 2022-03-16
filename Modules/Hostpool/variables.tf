e4dc41641bc66fb8c3a17473aa00a5def8a50315
JohanVanneuville <74196795+JohanVanneuville@users.noreply.github.com>
2022-03-16 20:57:10 +0100

    Update variables.tf

--- Modules/Hostpool/variables.tf
+++ Modules/Hostpool/variables.tf
@@ -1,7 +1,3 @@
-variable "location" {
-  description = "(Required) location where this resource has to be created"
-  default = "westeurope"
-}
 variable "prefix" {
     description = "customer prefix"
     default = "jvn"
