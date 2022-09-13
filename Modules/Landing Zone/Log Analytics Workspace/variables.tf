76501b2efd3db540c6e8124179f5ee9c00a93382
Johan Vanneuville <74196795+JohanVanneuville@users.noreply.github.com>
2022-09-13 16:46:25 +0200

    Update variables.tf

--- Modules/Landing Zone/Log Analytics Workspace/variables.tf
+++ Modules/Landing Zone/Log Analytics Workspace/variables.tf
@@ -2,12 +2,20 @@ variable "location" {
   description = "(Required) location where this resource has to be created"
   default = "westeurope"
 }
+
 variable "prefix" {
     description = "customer prefix"
     default = "jvn"
 }
-variable "environment" {
-    description = "prd,uat"
-    default = "prd"
+variable "env" {
+    description = "hub,prd,tst,dev,qa"
+    default = "hub"
+}
+variable "subscription_id_prd" {
+    default = "To be filled in"
+  
+}
+variable "subscription_id_mgmt" {
+    default = "To be filled in"
   
 }
