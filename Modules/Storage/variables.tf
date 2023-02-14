9a70d09b0338d9d564579548c911496a7e365c55
Johan Vanneuville <74196795+JohanVanneuville@users.noreply.github.com>
2023-02-14 07:39:45 +0100

    Update variables.tf

--- Modules/Storage/variables.tf
+++ Modules/Storage/variables.tf
@@ -1,8 +1,33 @@
 variable "location" {
   description = "(Required) location where this resource has to be created"
-  default = "westeurope"
+  default     = "westeurope"
 }
+
 variable "prefix" {
-    description = "customer prefix"
-    default = "jvn"
+  description = "customer prefix"
+  default     = "jvn"
 }
+variable "env" {
+  description = "hub,prd,tst,dev,qa"
+  default     = "prd"
+}
+variable "hub" {
+  default = "hub"
+}
+variable "subscription_id_prd" {
+  default = ""
+
+}
+variable "subscription_id_mgmt" {
+  default = ""
+
+}
+variable "solution" {
+  description = "can be sap,avd,..."
+  default     = "avd"
+
+}
+variable "subscription_id_avd" {
+  default = ""
+}
+
