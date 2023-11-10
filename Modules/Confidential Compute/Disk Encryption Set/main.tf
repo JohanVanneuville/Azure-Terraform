313dc2a902b83d3541d7e8f8dd489f9e22c494dd
Johan Vanneuville <74196795+JohanVanneuville@users.noreply.github.com>
2023-11-10 14:41:59 +0100

    Update main.tf

--- Modules/Confidential Compute/Disk Encryption Set/main.tf
+++ Modules/Confidential Compute/Disk Encryption Set/main.tf
@@ -1,3 +1,16 @@
+data "azurerm_resource_group" "rg-kv" {
+  provider = azurerm.hub
+  name = "rg-${var.env}-${var.prefix}-avd-management-01"
+}
+data "azurerm_key_vault" "kv" {
+    provider = azurerm.hub
+    name = "kv-${var.env}-${var.prefix}-avd-80"
+    resource_group_name = "rg-${var.env}-${var.prefix}-avd-management-01"
+}
+data "azurerm_key_vault_key" "avd-key" {
+  name = "key-prd-jvn-avd-10"
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
 resource "azurerm_disk_encryption_set" "en-set" {
     provider = azurerm.hub
   name                = "des-${var.env}-${var.prefix}-avd-50"
